76763ade644098dc16771fd40c29c7c8f389a726
Marc Becker <maarc1@gmx.de>
2022-09-28 11:30:37 +0200

    toggle function activate via usestate added (in ActiveToggle.js

--- src/components/ActiveToggle/ActiveToggle.js
+++ src/components/ActiveToggle/ActiveToggle.js
@@ -1,12 +1,32 @@
 import "./ActiveToggle.css";
+import { useState } from "react";
+
+
+
+
 
 function ActiveToggle() {
+
+  const [isActive, setActive] = useState(false);
+
+
+const handleToggle = () => {
+  setActive(!isActive);
+};
+
+
   return (
+
+    
     <main>
-      <div className="box" />
-      <button type="button">Activate</button>
+      <div className={isActive === false ? "box" : "box box--active"} />
+      <button type="button" onClick={handleToggle}>{isActive ===  false ? "Activate" : "Deactivate"}</button>
     </main>
   );
 }
+
+
+
+
 
 export default ActiveToggle;
